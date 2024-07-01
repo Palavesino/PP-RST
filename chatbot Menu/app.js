6577f6f8363842b8a56b8b1bf24858bf8c658453
Facundo <palavesinofacundo@gmail.com>
2024-07-01 20:09:40 -0300

    Delete comments

---
+++ chatbot Menu/app.js
@@ -0,0 +1,20 @@
+const { createBot, createProvider, createFlow} = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+const flowMenu = require('./Flow/Menu/Menu');
+const main = async () => {
+    const adapterDB = new MockAdapter()
+    const adapterFlow = createFlow(flowMenu)
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+    QRPortalWeb()
+}
+
+main()
