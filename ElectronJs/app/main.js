baf88184e16cd4bd03e9cd77594b5a7d00dd986d
Facundo <palavesinofacundo@gmail.com>
2024-07-18 19:33:17 -0300

    add Electron

---
+++ ElectronJs/app/main.js
@@ -0,0 +1,10 @@
+const { app, BrowserWindow } = require('electron');
+let mainWindow = null;
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    mainWindow.loadFile(__dirname + '/index.html');
+})
